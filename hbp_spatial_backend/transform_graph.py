48c0848365d3387afe306441d57ccece32adafaf
Yann Leprince <yann.leprince@cea.fr>
2019-02-28 12:04:38 +0100

    Introduce transform_graph module for a local transform graph

---
+++ hbp_spatial_backend/transform_graph.py
@@ -0,0 +1,95 @@
+# Copyright 2019 CEA
+# Author: Yann Leprince <yann.leprince@cea.fr>
+#
+# This file is part of hbp-spatial-backend.
+#
+# hbp-spatial-backend is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# hbp-spatial-backend is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with hbp-spatial-backend. If not, see <https://www.gnu.org/licenses/>.
+
+import collections
+
+
+class TransformGraph:
+    def __init__(self):
+        self.links = {}
+
+    def add_space(self, name):
+        name = str(name)
+        if name in self.links:
+            raise ValueError('there is already a space named {0}'.format(name))
+        self.links[name] = {}
+
+    def add_link(self, from_space, to_space, transform_file):
+        if (to_space in self.links[from_space]
+                or from_space in self.links[to_space]):
+            raise ValueError('{0} already has a link to {1}'.format(from_space,
+                                                                    to_space))
+
+        self.links[from_space][to_space] = transform_file
+
+    def remove_link(self, from_space, to_space):
+        del self.links[from_space][to_space]
+
+    def get_transform_chain(self, from_space, to_space):
+        # Trigger KeyError if the source or target space does not exist
+        self.links[from_space]
+        self.links[to_space]
+
+        to_visit = collections.deque([from_space])
+        visited = set()
+        back_pointers = {from_space: (None, None)}
+        # Breadth-first search on the spaces to find the shortest path
+        while True:
+            try:
+                space = to_visit.popleft()
+            except IndexError:
+                return None
+
+            # TODO detect ambiguities (multiple same-length chains)
+            if space == to_space:
+                chain = []
+                while back_pointers[space][0] is not None:
+                    space, transform = back_pointers[space]
+                    chain.append(transform)
+                chain.reverse()
+                return chain
+
+            for target_space, transform in self.links[space].items():
+                if target_space not in visited:
+                    visited.add(target_space)
+                    to_visit.append(target_space)
+                    back_pointers[target_space] = (space, transform)
+
+    def export_graphviz(self, f):
+        import string
+
+        def filter_name(name):
+            for c in name:
+                if c in string.digits + string.ascii_letters + '_':
+                    yield c
+                else:
+                    yield '_'
+
+        name_transforms = {name: ''.join(filter_name(name))
+                           for name in self.links}
+        target_names = set(name_transforms.values())
+        # check non-ambiguity of filtered names
+        assert len(target_names) == len(self.links)
+        f.write('digraph transforms {\n')
+        for name, targets in self.links.items():
+            for target_name in targets:
+                f.write('\t{0} -> {1};\n'.format(
+                    name_transforms[name],
+                    name_transforms[target_name]
+                ))
+        f.write('}\n')
---
+++ tests/test_transform_graph.py
@@ -0,0 +1,84 @@
+# Copyright 2019 CEA
+# Author: Yann Leprince <yann.leprince@cea.fr>
+#
+# This file is part of hbp-spatial-backend.
+#
+# hbp-spatial-backend is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# hbp-spatial-backend is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with hbp-spatial-backend. If not, see <https://www.gnu.org/licenses/>.
+
+import io
+
+import pytest
+
+from hbp_spatial_backend import transform_graph
+
+
+def test_transform_graph_sane_usage():
+    tg = transform_graph.TransformGraph()
+    tg.add_space('A')
+    chain = tg.get_transform_chain('A', 'A')
+    assert chain == []
+    tg.add_space('B')
+    chain = tg.get_transform_chain('A', 'B')
+    assert chain is None
+    tg.add_link('A', 'B', 'Transform A to B')
+    chain = tg.get_transform_chain('A', 'B')
+    assert chain == ['Transform A to B']
+    chain = tg.get_transform_chain('B', 'A')
+    assert chain is None
+    tg.add_space('C')
+    chain = tg.get_transform_chain('A', 'C')
+    assert chain is None
+    tg.add_link('B', 'C', 'Transform B to C')
+    chain = tg.get_transform_chain('A', 'C')
+    assert chain == ['Transform A to B', 'Transform B to C']
+    tg.remove_link('A', 'B')
+    chain = tg.get_transform_chain('A', 'C')
+    assert chain is None
+
+
+def test_transform_graph_cycle():
+    tg = transform_graph.TransformGraph()
+    tg.add_space('A')
+    tg.add_space('B')
+    tg.add_space('C')
+    tg.add_link('A', 'B', 'Transform A to B')
+    tg.add_link('B', 'C', 'Transform B to C')
+    tg.add_link('C', 'A', 'Transform C to A')
+    tg.add_space('D')
+    chain = tg.get_transform_chain('A', 'D')
+    assert chain is None
+
+
+def test_transform_graph_api_errors():
+    tg = transform_graph.TransformGraph()
+    tg.add_space('A')
+    with pytest.raises(ValueError):
+        tg.add_space('A')
+    with pytest.raises(KeyError):
+        tg.get_transform_chain('A', 'B')
+    with pytest.raises(KeyError):
+        tg.get_transform_chain('B', 'A')
+    tg.add_space('B')
+    tg.add_link('A', 'B', 'Transform A to B')
+    with pytest.raises(ValueError):
+        tg.add_link('A', 'B', 'Duplicate link')
+
+
+def test_export_graphviz():
+    tg = transform_graph.TransformGraph()
+    tg.add_space('A')
+    tg.add_space('weird"*+chars')
+    tg.add_link('A', 'weird"*+chars', 'Transform')
+    buf = io.StringIO()
+    tg.export_graphviz(buf)
