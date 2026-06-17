29a3b0e40b57342acd620b5f7c9b0b5c9eba816c
Denis Rivière <denis.riviere.gm@gmail.com>
2026-06-17 16:50:36 +0200

    removed use of "/packages" directory

--- docker-aims/script.sh
+++ docker-aims/script.sh
@@ -6,7 +6,7 @@
 # 1. Creates docker image         #
 ###################################
 
-DOCKER_IMAGE=brainvisa-aims:master_$(date -Id)
+DOCKER_IMAGE=brainvisa-aims:latest  # 6.0_$(date -Id)
 docker build -t $DOCKER_IMAGE --network=host .
 
 docker save -o $DOCKER_IMAGE.tar $DOCKER_IMAGE
